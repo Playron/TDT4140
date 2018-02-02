1ed90a59c256b23690398763525b82366cd8c414
brj <brj@10.22.37.141>
2018-02-02 15:51:33 +0100

    Dumb JUnit

---
+++ tdt4140-gr1805/app.ui/src/test/java/tdt4140/gr1805/app/ui/TestSuccess.java
@@ -0,0 +1,10 @@
+package tdt4140.gr1805.app.ui;
+
+import junit.framework.TestCase;
+
+public class TestSuccess extends TestCase {
+	public void testSuccess() throws Exception {
+		assertTrue(true);
+	}
+
+}
