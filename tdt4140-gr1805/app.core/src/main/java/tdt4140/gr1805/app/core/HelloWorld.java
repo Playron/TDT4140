36dc0e0675041849894d6394c3adf539c3e71eb9
perdh <perdh@stud.ntnu.no>
2018-02-14 16:18:04 +0100

    Lagde HelloWorld objekt

--- tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/HelloWorld.java
+++ tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/HelloWorld.java
@@ -3,8 +3,24 @@ package tdt4140.gr1805.app.core;
 public class HelloWorld {
 
 	
+	String helloworld;
+	
+	public HelloWorld(String helloworld) {
+		
+		this.helloworld = helloworld;
+	}
+	
+	public String getName() {
+		return this.helloworld;
+	}
+	
+	
+	public String toString() {
+		return "Navnet på dette objektet er: " + this.getName();
+	}
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		HelloWorld hello = new HelloWorld("Hei verden, smudesmyd");
+		System.out.println(hello.toString());
 	}
 }
