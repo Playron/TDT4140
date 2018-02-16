19c6837a6dbeae4ade5ffec1567cd0413088fcd8
perdh <perdh@stud.ntnu.no>
2018-02-16 14:48:57 +0100

    test av inputKonstruktør

---
+++ tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/CoreInput.java
@@ -0,0 +1,17 @@
+package tdt4140.gr1805.app.core;
+
+public class CoreInput {
+	
+	String id; int[][] lokasjon; int puls; String name; int alder;
+	
+	public CoreInput(String id, int[][] lokasjon, int puls, String name, int alder) {
+		this.id = id;
+		this.lokasjon = lokasjon;
+		this.puls = puls;
+		this.name = name;
+		this.alder = alder;
+	}
+
+	
+	
+}
