4ccaa3b15ae75d8c51268614f208d7c28397a602
perdh <perdh@stud.ntnu.no>
2018-02-16 15:22:55 +0100

    Mulighet for å legge til og fjerne bruker

--- tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/CoreInput.java
+++ tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/CoreInput.java
@@ -1,8 +1,12 @@
 package tdt4140.gr1805.app.core;
 
+import java.util.ArrayList;
+
 public class CoreInput {
 	
 	String id; int[][] lokasjon; int puls; String name; int alder;
+	ArrayList<CoreInput> brukere = new ArrayList<CoreInput>();
+	
 	
 	public CoreInput(String id, int[][] lokasjon, int puls, String name, int alder) {
 		this.id = id;
@@ -12,6 +16,24 @@ public class CoreInput {
 		this.alder = alder;
 	}
 
+	public void leggTilBruker(CoreInput nyBruker) {
+		if(!(brukere.contains(nyBruker))){
+			brukere.add(nyBruker);
+		}else {
+			throw new IllegalArgumentException("Brukeren er allerede registrert! ");
+		}
+	}
+	
+	public void fjernBruker(CoreInput bruker) {
+		if(brukere.contains(bruker)) {
+			brukere.remove(bruker);
+		} else {
+			throw new IllegalArgumentException("Brukeren er ikke registrert! ");
+		}
+	}
+	
+	
+	
 	
 	
 }
