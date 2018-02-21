aaea3159511fb37e06a6bd7f38acb2074f922b5f
annabakkeboe <annabakkeboe@dhcp-10-22-15-115.wlan.ntnu.no>
2018-02-21 14:20:24 +0100

    Testing commit

---
+++ tdt4140-gr1805/app.ui/src/test/java/tdt4140/gr1805/app/ui/TestSuccess1.java
@@ -0,0 +1,10 @@
+package tdt4140.gr1805.app.ui;
+
+import junit.framework.TestCase;
+
+public class TestSuccess1 extends TestCase {
+	public void testSuccess() throws Exception {
+		assertTrue(true);
+	}
+
+}
