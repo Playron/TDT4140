17af0eae6ec62007c2a21c872b024e1cecfeadb2
Torleif Naadland Hensvold <torleinh@stud.ntnu.no>
2018-02-26 10:59:14 +0100

    Created failing test for testbranch

---
+++ tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/CreateUserTest2.java
@@ -0,0 +1,10 @@
+package tdt4140.gr1805.app.core;
+
+import junit.framework.TestCase;
+
+public class CreateUserTest2 extends TestCase {
+	public void testSuccess() throws Exception {
+		assertTrue(false);
+	}
+
+}
