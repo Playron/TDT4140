9a5164100140333e1452e75d67ba17e5c86100da
Torleif Naadland Hensvold <torleinh@stud.ntnu.no>
2018-02-26 13:21:49 +0100

    Written test for Average Number that works so far: belongs to #2
    The test needs more work, and rework.

---
+++ tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/TestAverageNumber.java
@@ -0,0 +1,46 @@
+package tdt4140.gr1805.app.core;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class TestAverageNumber
+{
+	
+	
+	@Test
+	public void testZeroes()
+	{
+		
+		AverageNumber gmen = new AverageNumber();
+		List<Double> list1 = Arrays.asList(0.0, 0.0, 0.0, 0.0);
+		double result = gmen.getResult(list1);
+		assertEquals(0, result, 0);
+	}
+
+	public void testOnes()
+	{
+		AverageNumber gmen = new AverageNumber();
+		List<Double> list1 = Arrays.asList(1.0, 1.0, 1.0, 1.0);
+		double result = gmen.getResult(list1);
+		assertEquals(1, result, 0);
+	}
+	
+	public void testNoInput()
+	{
+		AverageNumber gmen = new AverageNumber();
+		List<Double> list1 = Arrays.asList();
+		double result = gmen.getResult(list1);
+		assertEquals(0, result, 0);
+	}
+	// TODO: Create package of testcases, which this runs through.
+}
