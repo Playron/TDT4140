85172c82afe933b18a37a9388cfee38a81d52e1c
Torleif Naadland Hensvold <torleinh@stud.ntnu.no>
2018-02-26 11:02:43 +0100

    Fixed naming of failing test in app.core test

--- tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/CreateUser2Test.java
+++ tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/CreateUser2Test.java
@@ -2,7 +2,7 @@ package tdt4140.gr1805.app.core;
 
 import junit.framework.TestCase;
 
-public class CreateUserTest2 extends TestCase {
+public class CreateUser2Test extends TestCase {
 	public void testSuccess() throws Exception {
 		assertTrue(false);
 	}
