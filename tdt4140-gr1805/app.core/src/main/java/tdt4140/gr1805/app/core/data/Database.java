98b02f526ad6736f23e3c8bd085e57f3c4c435a4
jonassa <jonas.sandb@gmail.com>
2018-03-07 15:53:03 +0100

    Implemented reading and writing to JSON-files #22

--- tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/data/Database.java
+++ tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/data/Database.java
@@ -1,16 +1,19 @@
 package tdt4140.gr1805.app.core.data;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import tdt4140.gr1805.app.core.person.*;
 
 public class Database {
 	
 	private ArrayList<Person> personList;
-	private ArrayList<DataPoint> dataPoints;
+	private ArrayList<DataPoint> dataPoints; // Add empty superclass default constructor to datapoint-class
 	private ArrayList<Workout> workouts;
 	private static File personDB;
 	private static File pointDB;
@@ -25,24 +28,28 @@ public class Database {
 		Database.personDB = new File("src/main/resources/tdt4140/gr1805/app/core/people.json");
 		Database.pointDB = new File("src/main/resources/tdt4140/gr1805/app/core/datapoints.json");
 		Database.workoutDB = new File("src/main/resources/tdt4140/gr1805/app/core/workouts.json");
+		// These probably need to be opened in another way, to not overwrite the files
 	}
 
-	public void readObjects() {
+	public void readObjects() throws JsonParseException, JsonMappingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		personList = mapper.readValue(personDB, new TypeReference<ArrayList<Person>>(){});
 		dataPoints = mapper.readValue(pointDB, new TypeReference<ArrayList<DataPoint>>(){});
 		workouts = mapper.readValue(workoutDB, new TypeReference<ArrayList<Workout>>(){});
-		
-		
 	}
 	
 	public void writeObjects() {
 		// Updates JSON files with current state of the lists.
 		// The app is however not expected to modify the database in any way,
 		// except maybe to add a Person/do something administrative.
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.writeValue(personDB, personList);
+		mapper.writeValue(pointDB, dataPoints);
+		mapper.writeValue(workoutDB, workouts);
+		
 	}
 	
-	public ArrayList<Person> getPersonList()
+	public ArrayList<Person> getPersonList() {
 		return personList;
 	}
 	
--- tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/data/Workout.java
+++ tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/data/Workout.java
@@ -11,6 +11,11 @@ public class Workout {
 	private Date endTime;
 	private ArrayList<DataPoint> dataPoints;
 	
+	// Nødvendig for å lage et Workout-objekt fra JSON.
+	public Workout() {
+		super();
+	}
+
 	public Workout(int id, String type, Date startTime, Date endTime, ArrayList<DataPoint> dataPoints) {
 		super();
 		this.id = id;
