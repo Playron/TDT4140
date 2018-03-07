d7b1dd2c2703f59e281705857620820b1a5a2e7e
Bjørn Rude Jacobsen <bjornrja@stud.ntnu.no>
2018-03-07 09:59:26 +0100

    Implemented DataGatheringLimits #8 #10

--- tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/data/DataGatheringLimits.java
+++ tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/data/DataGatheringLimits.java
@@ -1,19 +1,28 @@
 package tdt4140.gr1805.app.core.data;
 
 public class DataGatheringLimits{
-	//public DataGatheringLimits(){
-	private boolean GPS=true;
-	private boolean PULSE=true;
+	private boolean GPS;
+	private boolean PULSE;
+
+	public DataGatheringLimits(boolean GPS, boolean PULSE){
+		this.GPS=GPS;
+		this.PULSE=PULSE;
+	}
 
 	public void setGPSGathering(boolean GPSinput) {
-
+		GPS=GPSinput;
 	}
 
 	public void setPulseGathering(boolean PULSEinput) {
-
+		PULSE=PULSEinput;
 	}
 
-	public void getLimits(boolean GPS, boolean PULSE) {
-
+	public boolean getGPS() {
+		return GPS;
 	}
+	
+	public boolean getPULSE() {
+		return PULSE;
+	}
+	
 }
