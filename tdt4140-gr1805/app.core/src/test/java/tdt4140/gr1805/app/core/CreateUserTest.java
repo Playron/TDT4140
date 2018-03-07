64147b1fe3a83e658aa67367ba6970d3482611f2
perdh <perdh@stud.ntnu.no>
2018-03-07 10:57:07 +0100

    test commit

--- tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/CreateUserTest.java
+++ tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/CreateUserTest.java
@@ -1,7 +1,7 @@
 package tdt4140.gr1805.app.core;
 
 import junit.framework.TestCase;
-
+ //cool
 public class CreateUserTest extends TestCase {
 	public void testSuccess() throws Exception {
 		assertTrue(true);
