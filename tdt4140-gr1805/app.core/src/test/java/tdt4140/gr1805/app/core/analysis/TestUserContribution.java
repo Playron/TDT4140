1964cda6d686938d226da77059378f7359d4dbbb
Torleif Naadland Hensvold <torleinh@stud.ntnu.no>
2018-03-07 16:05:25 +0100

    Created failing test for User contribution.
    #1

---
+++ tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/analysis/TestUserContribution.java
@@ -0,0 +1,28 @@
+package tdt4140.gr1805.app.core.analysis;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestUserContribution
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
