a4ffcb63ee546b2a0dc1b1362367de3209391053
Bjørn Rude Jacobsen <bjornrja@stud.ntnu.no>
2018-03-07 10:23:44 +0100

    Updated shell and TODO for UpdateDataPoint

--- tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/data/UpdateDataPoint.java
+++ tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/data/UpdateDataPoint.java
@@ -1,20 +1,27 @@
 package tdt4140.gr1805.app.core.data;
 
 public class UpdateDataPoint extends DataGatheringLimits{
-	
-	
-	public UpdateDataPoint(boolean GPS, boolean PULSE) {
+
+
+	public UpdateDataPoint(boolean GPS, boolean PULSE, DataGatheringLimits DGL) {
 		super(GPS, PULSE);
-		// TODO Auto-generated constructor stub
+		//Auto-generated constructor stub
 	}
 
-	public void includePulse(String input){
-		
+	public void includePulse(){
+		//TODO
+		//if DGL.getPulse returns false, the update() method writes the attribute as NULL 
+
 	}
-	//public void includeGPS(boolean DataGatheringLimits.setGPSGathering()){}
-	
+
+	public void includeGPS(){
+		//TODO
+		//if DGL.getGPS returns false, the update() method writes the attribute as NULL 
+	}
+
 	public void update() {
-		
+		//checks if GPS and Pulse should be included
+		//method makes a datapoint with (time, pulse, GPS)-attributes and adds them to the database/file
 	}
-	
+
 }
