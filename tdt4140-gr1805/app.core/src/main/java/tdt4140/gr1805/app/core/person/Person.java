5af9eb6c5487332a25592e2a4f88b1eb8ab55e86
Torleif Naadland Hensvold <torleinh@stud.ntnu.no>
2018-03-07 10:46:29 +0100

    changed age to DOB #11
    Added setter for DOB in Person.java.

--- tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/person/Person.java
+++ tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/person/Person.java
@@ -1,22 +1,35 @@
 package tdt4140.gr1805.app.core.person;
 
+import java.time.*;
+
 public class Person {
 	
 	private int ID;
-	private int age;
+	private LocalDate DOB;
 	private static int id = 1;
 	private Gender gender;
 	
 	
-	public Person (int age, Gender gender) {
+	public Person (int year, int month, int day, Gender gender) {
 		
-		this.age = age;
+		setDOB(year, month, day);
 		this.gender = gender;
 		this.id = id;
 		id++;
 		System.out.println(gender);
 	}
 
+	private void setDOB(int year, int month, int day)
+	{
+		try
+		{
+			this.DOB = LocalDate.of(year, month, day);
+		}
+		catch (Exception e)
+		{
+			// TODO: handle exception
+		}
+	}
 
 	public int getID() {
 		return ID;
@@ -24,8 +37,14 @@ public class Person {
 
 
 	public int getAge() {
-		return age;
+		LocalDate dateNow = LocalDate.now();
+		return Period.between(this.DOB, dateNow).getYears();
 	}	
 	
-
+/*	public static void main(String[] args)
+	{
+		Person pers1 = new Person(1991, 02, 20, Gender.MALE);
+		System.out.println();
+	}
+*/
 }
