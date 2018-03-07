5d34c59bd2517b9eb75c0f79336037cbf1e3d335
Bjørn Rude Jacobsen <bjornrja@stud.ntnu.no>
2018-03-07 10:01:30 +0100

    Implemented DataGatheringLimits #8 #10

--- tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/data/UpdateDataPoint.java
+++ tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/data/UpdateDataPoint.java
@@ -3,6 +3,11 @@ package tdt4140.gr1805.app.core.data;
 public class UpdateDataPoint extends DataGatheringLimits{
 	
 	
+	public UpdateDataPoint(boolean GPS, boolean PULSE) {
+		super(GPS, PULSE);
+		// TODO Auto-generated constructor stub
+	}
+
 	public void includePulse(String input){
 		
 	}
