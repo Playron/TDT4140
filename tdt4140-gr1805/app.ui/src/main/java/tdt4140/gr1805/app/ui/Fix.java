4dc1635de88f89132ed41b12f4145deef3eea1e3
perdh <perdh@stud.ntnu.no>
2018-03-08 15:49:16 +0100

    Lagt Til registrationScreen UI #12

--- tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/Fix.java
+++
@@ -1,5 +0,0 @@
-package tdt4140.gr1805.app.ui;
-
-public class Fix {
-
-}
---
+++ tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/RegistrationScreen/Controller/registrationScreenController.java
@@ -0,0 +1,16 @@
+package tdt4140.gr1805.app.ui.RegistrationScreen.Controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class registrationScreenController extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
