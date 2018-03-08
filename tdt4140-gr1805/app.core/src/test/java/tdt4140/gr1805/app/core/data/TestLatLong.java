56fe290d93763cd94d139c0acef4e90ee8fbe7fc
Torleif Naadland Hensvold <torleinh@stud.ntnu.no>
2018-03-08 09:53:05 +0100

    Added failing test for LatLong.java
    #20

---
+++ tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/data/TestLatLong.java
@@ -0,0 +1,28 @@
+package tdt4140.gr1805.app.core.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestLatLong
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
