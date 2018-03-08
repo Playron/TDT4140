187182c35f806816db1e777610c8c88d1c9b5cdb
perdh <perdh@stud.ntnu.no>
2018-03-08 15:38:14 +0100

    Lagt til LoginScreen og LoginScreenController #12

---
+++ tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/LoginScreen/Controller/loginScreenController.java
@@ -0,0 +1,16 @@
+package tdt4140.gr1805.app.ui.LoginScreen.Controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class loginScreenController extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
