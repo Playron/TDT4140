539dbb06fa7fabe617baeda1621cea1da9fbfdb3
perdh <perdh@stud.ntnu.no>
2018-03-14 13:21:08 +0100

    Added functionality to add gender to profile

--- tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/registrationScreenController.java
+++ tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/registrationScreenController.java
@@ -1,8 +1,14 @@
 package tdt4140.gr1805.app.ui;
 
+import org.hamcrest.core.IsInstanceOf;
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.SelectionModel;
 import javafx.scene.control.TextField;
 import tdt4140.gr1805.app.ui.*;
 import tdt4140.gr1805.app.core.*;
@@ -12,6 +18,9 @@ import javafx.stage.Stage;
 
 public class registrationScreenController extends Application {
 
+	ObservableList<Gender> kjønnListe = FXCollections.observableArrayList(Gender.MALE, Gender.FEMALE); //Creates List with Gender-Enum
+	
+	
 	@FXML
 	TextField day_ID;
 	@FXML
@@ -20,16 +29,32 @@ public class registrationScreenController extends Application {
 	TextField year_ID;
 	@FXML
 	Label logInMessage;
+	@FXML
+	ChoiceBox kjønn = new ChoiceBox(kjønnListe);
+	
+	@FXML
+	public void initialize() {
+		kjønn.setItems(kjønnListe); 		//Initialize List
+
+	}
 	
 	
 	@FXML
 	public void registerUser() {
-		int dag = Integer.parseInt(day_ID.getText());
+		
+			//Next bit of code is parsing from textField to Int
+			//Need to write validation forinvalid inputs
+		int dag = Integer.parseInt(day_ID.getText());   			
 		int maaned = Integer.parseInt(month_ID.getText());
 		int aar = Integer.parseInt(year_ID.getText());
-		Person person = new Person(aar, maaned, dag, Gender.MALE);
-		System.out.println(person);
-		logInMessage.setText(person.toString());
+		
+		//Checks if choiceBox-choices is an instance of the Gender enum.
+		if(kjønn.getSelectionModel().getSelectedItem() instanceof Gender) {			
+			Person person = new Person(aar, maaned, dag, (Gender)kjønn.getSelectionModel().getSelectedItem()); //
+			System.out.println(person);
+		}else {
+			throw new IllegalArgumentException("Not an instance of Gender-Enum!");  //Should never trigger
+		}
 	}
 	
 	
