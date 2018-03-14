8f0ce9f0f17abc126988aa0ca114aa4f4138b9f3
jonassa <jonas.sandb@gmail.com>
2018-03-14 16:13:49 +0100

    Added Exercise enumeration for different types of workout.

---
+++ tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/data/Exercise.java
@@ -0,0 +1,8 @@
+package tdt4140.gr1805.app.core.data;
+
+public enum Exercise {
+	RUNNING,
+	CYCLING
+	//TODO: tenke ut av hva som gir mening å ha her. Trening samler lokasjon, men man kan jo trene stillestående og bare sende
+	// puls, og analysere bare pulsen.
+}
