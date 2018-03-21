8adc92001174021a837e835908e659000ffc2d2c
Torleif Naadland Hensvold <torleinh@stud.ntnu.no>
2018-03-21 14:16:58 +0100

    Created SettingsScreenController.java
    Copied setScreenController-method and the public MasterScreenController.
    #7 #9 #14 #16 #17 #15

---
+++ tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/SettingsScreenController.java
@@ -0,0 +1,15 @@
+package tdt4140.gr1805.app.ui;
+
+public class SettingsScreenController 
+{
+	public MasterScreenController screenController;
+	
+	
+	
+	
+	
+	public void setScreenController(MasterScreenController screenController) 
+	{
+		this.screenController = screenController;
+	}
+}
