108eada2cd748cc99a7a1002da72768ccf95eca9
TorleifHensvold <torleif.hensvold@outlook.com>
2018-03-21 11:38:45 +0100

    Added fields for items added in 5636ef5b4a6afd1e4a06b80d6da273f531e9734c
    #31 #19

--- tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/StatisticsScreenController.java
+++ tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/StatisticsScreenController.java
@@ -2,12 +2,18 @@ package tdt4140.gr1805.app.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class StatisticsScreenController 
 {
 	public MasterScreenController screenController;
 	@FXML
 	private Button showMeanButton;
+	@FXML
+	private TextField showMeanField;
+	@FXML
+	private TextArea statisticsScreenTextArea1;
 	
 	
 	
