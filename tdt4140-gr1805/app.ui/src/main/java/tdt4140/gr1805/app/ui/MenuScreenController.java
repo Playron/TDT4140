55fc089e1c5f1a985ee0bb32a7859b724255da33
Bjørn Rude Jacobsen <bjornrja@stud.ntnu.no>
2018-03-22 13:15:34 +0100

    Added fxml and controller for UserScreen, updated fxml and controller for MenuScreen, updated mainController
    #48
    #55

--- tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/MenuScreenController.java
+++ tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/MenuScreenController.java
@@ -24,6 +24,10 @@ public class MenuScreenController
 		screenController.activate("StatisticsScreen");
 	}
 	
+	@FXML
+	public void openUserScreen() {
+		screenController.activate("UserScreen");
+	}
 	
 	
 }
