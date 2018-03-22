1e8779f89fec45e06d9368f2b5b1bf32708008e9
annabakkebo <annabakk@stud.ntnu.no>
2018-03-22 10:13:25 +0100

    Created city enum #42
    To represent big cities in Norway

---
+++ tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/person/City.java
@@ -0,0 +1,11 @@
+package tdt4140.gr1805.app.core.person;
+
+public enum City {
+	TRONDHEIM,
+	OSLO,
+	TROMSØ,
+	BERGEN, 
+	STAVANGER,
+	KRISTIANSAND,
+	//TODO: add/delete cities to make a good representation of Norway
+}
