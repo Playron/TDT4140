89f89fd2b7385689b54464ca0ffd6b8f36bd679b
atle_ <atlevs@stud.ntnu.no>
2018-04-04 14:21:43 +0200

    Created class RecordActivityScreen in the package app.ui #61
    Nothing in the class by now.

---
+++ tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/RecordActivityScreenController.java
@@ -0,0 +1,11 @@
+package tdt4140.gr1805.app.ui;
+
+public class RecordActivityScreenController {
+	
+	public MasterScreenController screenController;
+	
+	public void setScreenController(MasterScreenController screencontroller) {
+		this.screenController = screenController;
+	}
+
+}
