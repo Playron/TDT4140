374b97548718fe06ce94f6a0755bb372186cd16c
atle_ <atlevs@stud.ntnu.no>
2018-04-04 15:34:03 +0200

    Added functionality to buttons #61
     BacktoMenu-button and showing the Record-screen when clicking on
    record-symbol in the MenuScreen

--- tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/RecordActivityScreenController.java
+++ tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/RecordActivityScreenController.java
@@ -1,11 +1,21 @@
 package tdt4140.gr1805.app.ui;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class RecordActivityScreenController {
 	
 	public MasterScreenController screenController;
 	
-	public void setScreenController(MasterScreenController screencontroller) {
+	public void setScreenController(MasterScreenController screenController) {
 		this.screenController = screenController;
+	}
+	
+	@FXML
+	public void backToMenu(ActionEvent event) throws IOException {
+		screenController.activate("MenuScreen");
 	}
 
 }
