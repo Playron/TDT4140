157297e703e697dec8a79008ce2e51a3eec4933f
Michael Moen Allport <mmallport@gmail.com>
2018-04-04 15:09:25 +0200

    Added adminscreen

--- tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/AdminScreenController.java
+++ tdt4140-gr1805/app.ui/src/main/java/tdt4140/gr1805/app/ui/AdminScreenController.java
@@ -1,9 +1,29 @@
 package tdt4140.gr1805.app.ui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+
+
 public class AdminScreenController {
+	@FXML
+	private ChoiceBox ByChoiceBox;
+	
 	public MasterScreenController screenController;
+	ObservableList<String> byListe = FXCollections.observableArrayList("Ingen","Trondheim","Oslo","Bergen","Stavanger","Kristiansand","Tromsoe");
+	
+	@FXML
+	private void initialize() {
+		ByChoiceBox.setValue("Ingen");
+		ByChoiceBox.setItems(byListe);
+		
+	}
+	
 	
 	public void setScreenController(MasterScreenController screenController) {
 		this.screenController = screenController;
 	}
+	
 }
