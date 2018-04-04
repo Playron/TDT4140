77d1b2490c01cab104970e4caffce1c1e150072f
Torleif Naadland Hensvold <torleinh@stud.ntnu.no>
2018-04-04 16:17:09 +0200

    #19 removed unused imports
    Removed 2 unused imports in TestPerson.java in test.app.core

--- tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/person/TestPerson.java
+++ tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/person/TestPerson.java
@@ -11,9 +11,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-import java.util.Set;
 
 public class TestPerson
 {
