16c40a841268691cf39198e027b09500a2629583
Torleif Naadland Hensvold <torleinh@stud.ntnu.no>
2018-04-05 08:47:09 +0200

    #19 Removed unused import
    Removed unused LatLong import.

--- tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/analysis/TestStatistics.java
+++ tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/analysis/TestStatistics.java
@@ -1,3 +1,4 @@
+
 package tdt4140.gr1805.app.core.analysis;
 
 import static org.junit.Assert.*;
@@ -9,13 +10,14 @@ import org.junit.Test;
 
 import tdt4140.gr1805.app.core.data.DataPoint;
 import tdt4140.gr1805.app.core.data.Exercise;
-import tdt4140.gr1805.app.core.data.LatLong;
 import tdt4140.gr1805.app.core.data.Workout;
 
-public class TestStatistics {
+public class TestStatistics
+{
 
 	@Test
-	public void testComputeMedian1() {
+	public void testComputeMedian1()
+	{
 		ArrayList<Double> liste = new ArrayList<Double>();
 		liste.add(2.9);
 		liste.add(2.1);
@@ -24,9 +26,10 @@ public class TestStatistics {
 		Double actual = 2.3;
 		assertEquals(median, actual);
 	}
-	
+
 	@Test
-	public void testComputeMedian2() {
+	public void testComputeMedian2()
+	{
 		ArrayList<Double> liste = new ArrayList<Double>();
 		liste.add(2.9);
 		liste.add(2.1);
@@ -36,9 +39,10 @@ public class TestStatistics {
 		Double actual = 2.2;
 		assertEquals(median, actual);
 	}
-	
+
 	@Test
-	public void testmostUsedExercise1() {
+	public void testmostUsedExercise1()
+	{
 		ArrayList<Workout> liste1 = new ArrayList<Workout>();
 		Date dato = new Date(0);
 		double tall = 70;
@@ -46,19 +50,21 @@ public class TestStatistics {
 		ArrayList<DataPoint> dpa = new ArrayList<DataPoint>();
 		dpa.add(point);
 		dpa.add(point);
-		Workout w = new Workout(2,Exercise.RUNNING, dpa);
+		Workout w = new Workout(2, Exercise.RUNNING, dpa);
 		liste1.add(w);
-		for (int i = 0; i < 4; i++) {
-			Workout a = new Workout(2,Exercise.CYCLING, dpa);
+		for (int i = 0; i < 4; i++)
+		{
+			Workout a = new Workout(2, Exercise.CYCLING, dpa);
 			liste1.add(a);
 		}
 		Exercise expected = Exercise.CYCLING;
 		Exercise mostused = Statistics.mostUsedExercise(liste1);
 		assertEquals(expected, mostused);
 	}
-	
+
 	@Test
-	public void testmostUsedExercise2() {
+	public void testmostUsedExercise2()
+	{
 		ArrayList<Workout> liste1 = new ArrayList<Workout>();
 		Date dato = new Date(0);
 		double tall = 70;
@@ -66,10 +72,11 @@ public class TestStatistics {
 		ArrayList<DataPoint> dpa = new ArrayList<DataPoint>();
 		dpa.add(point);
 		dpa.add(point);
-		Workout w = new Workout(2,Exercise.RUNNING, dpa);
+		Workout w = new Workout(2, Exercise.RUNNING, dpa);
 		liste1.add(w);
-		for (int i = 0; i < 4; i++) {
-			Workout a = new Workout(2,Exercise.RUNNING, dpa);
+		for (int i = 0; i < 4; i++)
+		{
+			Workout a = new Workout(2, Exercise.RUNNING, dpa);
 			liste1.add(a);
 		}
 		Exercise expected = Exercise.RUNNING;
