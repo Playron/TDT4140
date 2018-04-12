d24441411a77abf6bb7507e6c833dda32b1dd7ae
atle_ <atlevs@stud.ntnu.no>
2018-04-12 15:59:19 +0200

    Created class TestWorkout for testing Workout #66
    By now it tests the method CalculateAverageExercisePulse

--- tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/data/TestWorkout.java
+++ tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/data/TestWorkout.java
@@ -2,21 +2,29 @@ package tdt4140.gr1805.app.core.data;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
+
 import org.junit.Test;
 
-public class TestWorkout
-{
+public class TestWorkout {
+	
+	private Database db;
+	private ArrayList<Workout> workouts;
+	Workout w0, w1, w2, w3, w4, w5;
 
 	@Before
-	public void setUp() throws Exception
-	{
-	}
-
-	@After
-	public void tearDown() throws Exception
-	{
+	public void setUp() throws Exception {
+		db = new Database();
+		workouts = db.getAllWorkouts();
+		w0 = workouts.get(0);
+		w1 = workouts.get(1);
+		w2 = workouts.get(2);
+		w3 = workouts.get(3);
+		w4 = workouts.get(4);
+		w5 = workouts.get(5);
 	}
 
 	@Test
@@ -24,5 +32,13 @@ public class TestWorkout
 	{
 		assertTrue(true);
 	}
+	
+	@Test
+	public void testCalculateAvgExercisePulse() {
+		
+		assertEquals(100.19, w1.calculateAverageExcercisePulse(),0.1);
+	}
+	
 
+	
 }
