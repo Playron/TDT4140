8720067d6ef98d522224f0b9890e23d932fc51a5
atle_ <atlevs@stud.ntnu.no>
2018-04-16 13:19:41 +0200

    Updated TestWorkout class #66
    Now the setup-method creates a workout

--- tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/data/TestWorkout.java
+++ tdt4140-gr1805/app.core/src/test/java/tdt4140/gr1805/app/core/data/TestWorkout.java
@@ -2,6 +2,7 @@ package tdt4140.gr1805.app.core.data;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import org.junit.After;
@@ -10,26 +11,38 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestWorkout {
-	
-	private Database db;
-	private ArrayList<Workout> workouts;
-	Workout w0, w1, w2, w3, w4, w5;
+
+	private Workout w;
+	private DataPoint p1, p2, p3, p4, p5 ;
 
 	@Before
 	public void setUp() throws Exception {
-		db = new Database();
-		workouts = db.getAllWorkouts();
-		w0 = workouts.get(0);
-		w1 = workouts.get(1);
-
+		
+	LocalDateTime man16 =LocalDateTime.of(2018, 4, 16, 13, 0);
+	w = new Workout(1, Exercise.RUNNING);
+	p1 = new DataPoint(1, man16, 90);
+	p2 = new DataPoint(1, man16.plusMinutes(10), 95);
+	p3 = new DataPoint(1, man16.plusMinutes(20), 100);
+	p4 = new DataPoint(1, man16.plusMinutes(30), 103);
+	p5 = new DataPoint(1, man16.plusMinutes(40), 107);
+	w.addDataPoint(p1);
+	w.addDataPoint(p2);
+	w.addDataPoint(p3);
+	w.addDataPoint(p4);
+	w.addDataPoint(p5);
 	}
 
 
 	@Test
 	public void testCalculateAvgExercisePulse() {
+		double excpected = 99;
+		assertEquals(excpected, w.calculateAverageExcercisePulse(),0.1);
+	}
+	
+	@Test
+	public void testCalculateDistance() {
 		
-		double excpected = w1.calculateAverageExcercisePulse();
-		assertEquals(excpected, w1.calculateAverageExcercisePulse(),1);
+		
 	}
 	
 	
