952d1930228c8ae5c5cd3fcac57422998136df11
perdh <perdh@stud.ntnu.no>
2018-04-16 15:24:33 +0200

    Readded randomclass

--- tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/Random.java
+++ tdt4140-gr1805/app.core/src/main/java/tdt4140/gr1805/app/core/Random.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
-/*public final class Random {
+public final class Random {
     private static final java.util.Random R = new java.util.Random();
     private static final List<City> CITIES = Arrays.asList(City.values());
     private static final List<Exercise> EXERCISES = Arrays.asList(Exercise.values());
@@ -85,4 +85,3 @@ import java.util.List;
     }
 
 }
-*/
